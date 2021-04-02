becac0ab2331161bf55ccecbd1dfac92fbf25987
Tamm <tamm.sjodin@gmail.com>
2021-04-02 13:26:39 +1100

    feat: add basic api

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # cautious-octo-disco
+
+WCGW?
+
+## Installation / Getting it started
+
+Just navigate to this folder and run
+
+```
+npm install
+npm start
+```
---
+++ src/models/dotenvSchema.ts
@@ -0,0 +1,30 @@
+import { EnvType, load } from "ts-dotenv";
+
+export type Env = EnvType<typeof schema>;
+
+export const schema = {
+  PORT: {
+    type: Number,
+    optional: true,
+    default: 8080,
+  },
+  APP_NAME: {
+    type: /^[-a-z]+$/,
+    optional: true,
+  },
+  BASE_URL: {
+    type: String,
+    optional: true,
+  },
+  API_KEY: {
+    type: String,
+    optional: true,
+  },
+  NODE_ENV: ["production" as const, "development" as const],
+};
+
+export let env: Env;
+
+export function loadEnv(): void {
+  env = load(schema);
+}
